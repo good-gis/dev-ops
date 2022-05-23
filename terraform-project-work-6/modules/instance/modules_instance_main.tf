b1722c1b657136f0165ca6238abad25f140235a7
Ivan Glushkov <glushkov1997@icloud.com>
2022-05-24 00:31:54 +0300

    init

--- terraform-project-work-6/modules/instance/modules_instance_main.tf
+++ terraform-project-work-6/modules/instance/modules_instance_main.tf
@@ -32,6 +32,6 @@ resource "yandex_compute_instance" "vm" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
 }
